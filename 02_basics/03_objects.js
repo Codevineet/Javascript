8165cc5fdb79fe13732514d90da3ec5b73cd99b0
Vineet Jindal <99903469+Codevineet@users.noreply.github.com>
2024-03-02 17:03:26 +0000

    object basic 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,41 @@
+//singleton
+//if we have to add a symbol in the object then use [] in the key and write the symbol name inside it
+const mySym = Symbol("x");
+const jsUser = {
+    name: "Vineet",
+    "full-name":"Vineet jindal",
+    age:20,
+    [mySym] : "lol",
+    email: "jindal@gmail.com",
+    isLoggedIn: false,
+    lastLoggedIn : ["Monday" , "Tuesday"],
+}
+
+
+
+// console.log(jsUser.name);
+// console.log(jsUser["full-name"]);
+// console.log(jsUser["email"]);
+// console.log(jsUser[mySym]);
+// console.log(jsUser);
+
+const func = {
+    greeting : function (){
+        console.log("Hello there!!!");
+    }
+}
+
+func.greeting2 = function (){
+    console.log("Hello mates");
+}
+
+jsUser.greeting = function (){
+    console.log(`welcome again ${this["full-name"]}`);
+}
+
+console.log(jsUser.greeting());
+
+
+// console.log(func);
+// console.log(func.greeting);
+// console.log(func.greeting2());
