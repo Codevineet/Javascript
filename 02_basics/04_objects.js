ab68935b5dcae21debbeed04c7f477be88b86d0d
Vineet Jindal <99903469+Codevineet@users.noreply.github.com>
2024-03-03 06:27:20 +0000

    object 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,52 @@
+//singleton object
+const user = new Object();
+
+//non singleton object
+const user2 = {};
+
+
+//we will be doing everthing on singleton object
+user.name = "Vineet Jindal";
+user.id = "123ab";
+user.isLoggedIn = false;
+
+//passing a object in the object
+user.address = {
+    add1:"shastri nagar",
+    add2: "Tri nagar",
+}
+
+// console.log(user.address.add1);
+
+//now merging to objects
+const obj1 = {
+    1: 1,
+    2:2,
+    3:3
+}
+
+const obj2 = {
+    4:4,
+    5:5,
+}
+
+// const obj3 = Object.assign({} , obj1 , obj2);
+const obj3 = {...obj1 , ...obj2};
+// console.log(obj3);
+
+const emails = [
+    {
+        id:"1",
+        name:"harsh"
+    },
+    {
+        id:"2",
+        name:"Vineet",
+    }
+]
+
+console.log(emails[0].id);
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+
