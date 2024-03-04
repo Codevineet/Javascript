bf22d3cf56dd6cb6e7d1b3254df82eb757ca9271
Vineet Jindal <99903469+Codevineet@users.noreply.github.com>
2024-03-04 12:58:53 +0000

    arrow function

---
+++ 03_basics/02_arrow_function.js
@@ -0,0 +1,45 @@
+const user = {
+    name: "Vineet Jindal",
+    age: 20,
+
+    welcomeMessage : function(){
+        console.log(`${this.name}, welcome to the website`)
+    }
+}
+
+
+// console.log(user.welcomeMessage());
+
+// const chai = function(){
+//     let username = "Vineet Jindal";
+//     console.log(this.username);
+// }
+
+// chai();
+//giving undefined
+
+// const chai = (num1 , num2) =>{
+//     let username = "Vineet Jindal";
+//     console.log(this);
+//     return num1 + num2;
+// }
+
+const arrowFunction = (name) =>{
+    return {myname: name};
+}
+
+console.log(arrowFunction('Vineet Jindal'));
+
+
+//IIFE concept immediately Invoked Function Expression
+//because of the global pollution and it may effect the other things, that's why we use IIFE
+
+(function db(){
+    //named IIFE
+    console.log("DB is connected....")
+})();
+
+((name) =>{
+    //unnamed IIFE
+    console.log(`DB is connected ${name}`)
+})("Mr. Vineet Jindal");
